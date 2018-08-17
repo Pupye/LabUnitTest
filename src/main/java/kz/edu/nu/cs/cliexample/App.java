1a1ce07b82be345cc0c643b137371534a6dbd31e
marks <marks1024@users.noreply.github.com>
2018-08-17 08:37:55 +0600

    edits

--- src/main/java/kz/edu/nu/cs/cliexample/App.java
+++ src/main/java/kz/edu/nu/cs/cliexample/App.java
@@ -1,11 +1,42 @@
 package kz.edu.nu.cs.cliexample;
 
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * 
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        
+        CommandLineParser parser = new DefaultParser();
+        Options options = new Options();
+        options.addOption("help", false, "print this help message");
+        options.addOption("s", true, "input string");
+        
+        CommandLine cmd;
+        
+        try {
+            cmd = parser.parse(options, args);
+            
+            if (cmd.hasOption("help")) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("App", options);
+                System.exit(0);
+            }
+            
+            if (cmd.hasOption("s")) {
+                String s = cmd.getOptionValue("s");
+                System.out.println(s);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     public static int wordcount(String s) {
