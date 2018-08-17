e988b90a8b7c3b98018196c377fff27d5d66b392
marks <marks1024@users.noreply.github.com>
2018-08-17 08:05:30 +0600

    edits

---
+++ src/main/java/kz/edu/nu/cs/cliexample/App.java
@@ -0,0 +1,16 @@
+package kz.edu.nu.cs.cliexample;
+
+/**
+ * 
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+    public static int wordcount(String s) {
+        // Assume s is a sentence and count the number of words
+        // Complete implementation
+        return 0;
+    }
+}
